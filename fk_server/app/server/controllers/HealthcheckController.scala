56e807eb78cb7083666869baaf7270d79b142389
Bernd Huber <45738360+funkrusher@users.noreply.github.com>
2023-10-04 20:52:01 +0200

    changes
    changes

--- fk_server/app/server/controllers/HealthcheckController.scala
+++ fk_server/app/server/controllers/HealthcheckController.scala
@@ -1,5 +1,10 @@
 package server.controllers
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParams
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import play.api.i18n.I18nSupport
 import play.api.mvc.AbstractController
 import play.api.mvc.ControllerComponents
@@ -8,6 +13,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import scala.concurrent.Future
 
+@Api("Healthcheck")
 @Singleton
 class HealthcheckController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
 
@@ -16,6 +22,14 @@ class HealthcheckController @Inject() (cc: ControllerComponents) extends Abstrac
    *
    * @return success of healthcheck
    */
+  @ApiOperation(
+    value = "Healthcheck"
+  )
+  @ApiResponses(
+    Array(
+      new ApiResponse(code = 200, message = "The Healthcheck was executed successfully")
+    )
+  )
   def healtcheck() =
     Action.async { implicit request =>
       Future.successful(Ok("Rockin' in the Free World!"))
